b25b3d9dabce7721510d9925f7d200dd0951d5c4
brassica-oleracea <71041795+brassica-oleracea@users.noreply.github.com>
2020-09-17 23:13:36 -0500

    Added the total count of requests
    total amount of requests will not be output

--- temp.py
+++ temp.py
@@ -13,3 +13,10 @@ LOCAL_FILE = 'local_copy.log'
 
 # Use urlretrieve() to fetch a remote copy and save into the local file path
 local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE)
+#This retrieves the log data, input your file path to the log copy on your local machine.
+#This part of the script counts the total requests in the log.
+FILE_NAME = 'C:/Users/noahr/.spyder-py3/local_copy.log'
+count = 0
+for line in open(FILE_NAME):
+    count = count + 1
+print('The number of total requests is' ,count)
